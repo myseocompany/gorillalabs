6b9be851dcfec34cba868253453b7c3ab70e7c5a
Nicolás Navarro <nicolas@myseocompany.co>
2024-10-29 09:39:06 -0500

    add favicons

--- resources/views/result.blade.php
+++ resources/views/result.blade.php
@@ -3,11 +3,17 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Resultados de Búsqueda</title>
+    <title>Gorilla Labs - Resultados de Búsqueda</title>
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-    <!-- Styles -->
+        <!-- Favicons -->
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/img/favicon_io/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/img/favicon_io/favicon-16x16.png">
+        <link rel="manifest" href="/img/favicon_io/site.webmanifest">
+        
+        <!-- Styles -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
