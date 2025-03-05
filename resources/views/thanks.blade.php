6d414b410963c898badd3074831e2444cdb84eeb
Nicolás Navarro <nicolas@myseocompany.co>
2025-03-05 17:23:40 -0500

    evento GA4

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -10,6 +10,7 @@
 
     gtag('config', 'G-3YSYGB9M1W');
     </script>
+
     <!-- Google Tag Manager -->
         <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
         new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
@@ -17,6 +18,16 @@
         'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
         })(window,document,'script','dataLayer','GTM-WZ2VGV5Z');</script>
     <!-- End Google Tag Manager -->
+
+    <!-- Event snippet for load_thanks conversion page -->
+    <script>
+        gtag('event', 'conversion', {
+            'send_to': 'AW-16757419033/TRINCN_Jw6YaEJnYx7Y-',
+            'value': 1.0,
+            'currency': 'COP'
+        });
+    </script>
+  
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
